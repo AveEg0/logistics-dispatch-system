cca82556081288e137c5dd1727d9702ded4e34e6
Oleksandr Karmazyn <karmazino548@gmail.com>
2026-04-21 00:00:16 -0500

    Initial project setup (Spring Boot, dependencies added)

---
+++ src/main/java/com/karmazyn/logisticsdispatchsystem/LogisticsDispatchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.karmazyn.logisticsdispatchsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsDispatchSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsDispatchSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karmazyn/logisticsdispatchsystem/LogisticsDispatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karmazyn.logisticsdispatchsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsDispatchSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
